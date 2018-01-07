4ca0268a3f0de5ea439cc92b684c5e554e816429
vanis <vanissoft@gmail.com>
2018-01-07 13:37:15 +0100

    Depth and trades charts in Market Panels module.

--- app/wmodmarketpanels.py
+++ app/wmodmarketpanels.py
@@ -20,7 +20,7 @@ Panel_count = 0
 
 def save_layout():
 	global Comm, Panels
-	spanels = [[Panels[p][k] for k in ['market', 'column']] for p in Panels]
+	spanels = [[Panels[p][k] for k in ['market', 'column', 'chart_type']] for p in Panels]
 	Comm.send({'operation': 'enqueue', 'module': "marketpanels", 'what': 'marketpanels_savelayout', 'data': json.dumps(spanels)})
 
 
@@ -38,14 +38,18 @@ def drag_receive(ev, ui):
 	print("panels:", Panels)
 	save_layout()
 
-def query_market(mkt):
+
+def query_make(mkt, chart_type):
 	global Comm
-	Comm.send({'operation': 'enqueue', 'module': Module_name, 'what': 'get_market_trades', 'market': mkt,
-		'date_from': (datetime.datetime.now() - datetime.timedelta(days=30)).isoformat(),
-		'date_to': datetime.datetime.now().isoformat()})
+	if chart_type == "trades":
+		Comm.send({'operation': 'enqueue_bg', 'module': Module_name, 'what': 'get_market_trades', 'market': mkt,
+			'date_from': (datetime.datetime.now() - datetime.timedelta(days=30)).isoformat(),
+			'date_to': datetime.datetime.now().isoformat()})
+	elif chart_type == "depth":
+		Comm.send({'operation': 'enqueue_bg', 'module': Module_name, 'what': 'orderbook', 'market': mkt})
 
 
-def new_panel(market, column):
+def new_panel(market, column, chart_type):
 	"""
 	Create a new panel in column 0
 	"""
@@ -57,47 +61,46 @@ def new_panel(market, column):
 	Panels[str(Panel_count)]['market'] = market
 	Panels[str(Panel_count)]['column'] = column
 	Panels[str(Panel_count)]['echart_obj'] = None
+	Panels[str(Panel_count)]['chart_type'] = chart_type
 	document['newpanel_'+column].outerHTML = p1
 	jq("#form" + str(Panel_count)).hide()
-	document["bChangeMarket1_" + str(Panel_count)].bind("click", click_change_market1)
-	document["bChangeMarket2_" + str(Panel_count)].bind("click", click_change_market2)
+	document["bChangeMarket1_" + str(Panel_count)].bind("click", click_new_trades)
+	document["bChangeMarket2_" + str(Panel_count)].bind("click", click_new_depth)
 	document["bFilter_" + str(Panel_count)].bind("click", click_show)
-	query_market(market)
+	document["bClose_" + str(Panel_count)].bind("click", click_close)
+	query_make(market, chart_type)
 
 
-def populate_panels():
-	# TODO: recover from database
-	Comm.send({'operation': 'enqueue', 'module': "marketpanels", 'what': 'marketpanels_loadlayout'})
-	#new_panel("OPEN.ETH/BTS", 1)
 
+def click_close(ev):
+	id = str(ev.target.id.split("_")[1])
+	print("close", ev.target.id)
+	print("close", id)
+	#jq("#form"+id).toggle("fast")
 
 def click_show(ev):
 	id = str(ev.target.id.split("_")[1])
 	jq("#form"+id).toggle("fast")
 
 
-def click_change_market1(ev):
+def click_new_trades(ev):
 	"""
-	Change market of the panel
-	:param ev:
-	:return:
-	"""
-	global Panels
-	id = str(ev.target.id.split("_")[1])
-	Panels[id]['market'] = document['iMarket_'+id].value
-	query_market(Panels[id]['market'])
-	jq("#form"+id).hide("fast")
-
-
-def click_change_market2(ev):
-	"""
-	Create a new panel
 	:param ev:
 	:return:
 	"""
 	id = str(ev.target.id.split("_")[1])
 	jq("#form"+id).hide()
-	new_panel(document['iMarket_'+id].value, Panels[id]['column'])
+	new_panel(document['iMarket_'+id].value, Panels[id]['column'], "trades")
+
+
+def click_new_depth(ev):
+	"""
+	:param ev:
+	:return:
+	"""
+	id = str(ev.target.id.split("_")[1])
+	jq("#form"+id).hide()
+	new_panel(document['iMarket_'+id].value, Panels[id]['column'], "depth")
 
 
 def init(comm):
@@ -106,7 +109,7 @@ def init(comm):
 	Panels = {}
 	Panel_count = 0
 	Comm = comm
-	populate_panels()
+	Comm.send({'operation': 'enqueue', 'module': "marketpanels", 'what': 'marketpanels_loadlayout'})
 	jq('.draggable-container [class*=col]').sortable({"handle": ".panel-body", "connectWith": "[class*=col]",
 		"receive": drag_receive,
 		"tolerance": 'pointer', "forcePlaceholderSize": True, "opacity": 0.8}).disableSelection()
@@ -119,33 +122,34 @@ def panel(id, mkt, column):
 	       <div class="panel-heading">
                 <div class="panel-tools">
                     <a role="button" id="bFilter_{0}" class="fa fa-filter"></a>
+                    <a role="button" id="bClose_{0}" class="fa fa-times"></a>
                 </div>
                 {1}
             </div>
             <div class="panel-body">
                 <div id="form{0}" class="input-group">
-                    <input type="text" class="form-control width100" id="iMarket_{0}" placeholder="xxx/xxx">
-                    <span class="input-group-btn">
-                        <button id="bChangeMarket1_{0}" class="btn btn-accent">Change</button>
-                        <button id="bChangeMarket2_{0}" class="btn btn-accent">New Panel</button>
-                    </span>
-                </div>
+	                <div class="input-group">
+	                    <input type="text" class="form-control width100" id="iMarket_{0}" placeholder="xxx/xxx">
+	                    <span class="input-group-btn">
+	                        <button id="bChangeMarket1_{0}" class="btn btn-accent">Trades</button>
+	                        <button id="bChangeMarket2_{0}" class="btn btn-accent">Depth</button>
+	                    </span>
+	                </div>
+	            <hr>
+	            </div>
                 <div id="echart_{0}" style="width:100%; height:250px; display: none;"></div>
             </div>
         </div>"""
 	return html.format(str(id), mkt, column)
 
 
-def onResize():
-	pass
 
 def incoming_data(data):
 	global Panels
-	print("incoming", data['data'])
 	if 'market_trades' in data['data']:
 		market = data['data']['market_trades']['market']
 		for p in Panels:
-			if Panels[p]['market'] == market:
+			if Panels[p]['market'] == market and Panels[p]['chart_type'] == "trades":
 				jq("#echart_"+p).show()
 				obj = window.echarts.init(document.getElementById("echart_"+p))
 				og = w_mod_graphs.MarketTrades1(obj)
@@ -154,8 +158,29 @@ def incoming_data(data):
 				og.load_data(data['data']['market_trades']['data'])
 				Panels[p]['echart_obj'] = obj
 				Panels[p]['wmgraph_obj'] = og
+
+	elif 'orderbook' in data['data']:
+		market = data['data']['orderbook']['market']
+		maxv = data['data']['orderbook']['data'][0][3]
+		data['data']['orderbook']['data'].insert(0, ['buy', 0, 0, maxv])
+		for p in Panels:
+			if Panels[p]['market'] == market and Panels[p]['chart_type'] == "depth":
+				jq("#echart_"+p).show()
+				obj = window.echarts.init(document.getElementById("echart_"+p))
+				og = w_mod_graphs.OrderBook1(obj)
+				og.title = market + " orderbook"
+				og.market = market
+				#og.orders = Order_pos[market]
+				og.load_data(data['data']['orderbook']['data'])
+				Panels[p]['echart_obj'] = obj
+				Panels[p]['wmgraph_obj'] = og
+
+
 	elif 'marketpanels_layout' in data['data']:
 		print(data['data']['marketpanels_layout'])
 		for panel in data['data']['marketpanels_layout']:
 			print(panel)
-			new_panel(panel[0], panel[1])
+			if len(panel) < 3:
+				new_panel(panel[0], panel[1], "depth")
+			else:
+				new_panel(panel[0], panel[1], panel[2])
